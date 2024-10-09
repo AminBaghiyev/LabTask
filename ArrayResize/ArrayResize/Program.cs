4c6be8ea37e69a8f74bb3a7a33e3c9bad82e8c79
Amin Baghiyev <aminib-ab205@code.edu.az>
2024-10-09 11:26:10 +0400

    ArrayResize task added

---
+++ ArrayResize/ArrayResize/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array1 = [1, 2, 3];
+int length = 10;
+int[] array2 = new int[length];
+
+for (int i = 0; i < array1.Length; i++)
+{
+    array2[i] = array1[i];
+}
+
+foreach (int i in array2)
+{
+    Console.Write($"{i} ");
+}
