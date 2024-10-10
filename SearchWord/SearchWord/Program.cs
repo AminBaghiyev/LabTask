50ec64489b5f7b85d2785269e1a50e2809a7016a
Amin <aminib-ab205@code.edu.az>
2024-10-10 15:28:46 +0400

    SearchWord task added

---
+++ SearchWord/SearchWord/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SearchWord;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.Write("Mətn daxil edin: ");
+        string textInput = Console.ReadLine();
+        Console.Write("Axtarılacaq sözü daxil edin: ");
+        string searchInput = Console.ReadLine();
+        bool response = SearchWord(textInput, searchInput);
+
+        if (response)
+            Console.WriteLine($"{searchInput} tapıldı");
+        else
+            Console.WriteLine($"{searchInput} tapılmadı");
+    }
+
+    public static bool SearchWord(string text, string search)
+    {
+        for (int i = 0; i <= text.Length - search.Length; i++)
+        {
+            if (text.Substring(i, search.Length) == search) return true;
+        }
+        return false;
+    }
+}
