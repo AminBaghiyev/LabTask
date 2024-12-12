b4647f5c5c07c00446dd262012b2887653fc72a6
Amin Baghiyev <aminib-ab205@code.edu.az>
2024-12-13 00:53:19 +0400

    Game CRUD Added

---
+++ GameStore/GameStore.DAL/Contexts/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GameStore.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.DAL.Contexts;
+
+public class AppDbContext : IdentityDbContext<AppUser, IdentityRole, string>
+{
+    public DbSet<Game> Games { get; set; }
+
+    public AppDbContext(DbContextOptions options) : base(options) { }
+}
---
+++ GameStore/GameStore.DAL/Migrations/20241211140941_GamesTableCreated.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class GamesTableCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ThumbnailPath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
